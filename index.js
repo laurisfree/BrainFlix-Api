c5fffd2b6b28c85836c68338fc91866e8738ebee
laurisfree <freelau00@gmail.com>
2022-11-18 17:37:46 -0800

    upload video

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const fs = require("fs");
 const cors = require ("cors");
+const { randomUUID } = require("crypto");
 const app = express();
+const {v4: uuid} = require('uuid')
 
 
 app.use (cors ());
@@ -37,9 +39,29 @@ app.get("/videos", function (req, res){
 });
 
 app.post("/videos", function (req, res){
+    const videosData = fs.readFileSync("./data/videos.json", "utf-8") 
+    const parseData = JSON.parse(videosData)
+    if (req.body?.title && req.body?.description) 
+    {parseData.push ({
+        id: uuid(),
+        title: req.body.title,
+        channel: "Fun channel",
+        image: "",
+        description: req.body.description,
+        views: "1,001,023",
+        likes: "110,985",
+        duration: "4:01",
+        video: "https://project-2-api.herokuapp.com/stream",
+        timestamp: Date.now(),
+        comments: [],
+    })
+
+        fs.writeFileSync("./data/videos.json", JSON.stringify(parseData))
+    }
     console.log(req.body)
     res.send("video added")
 })
+
 
 
 app.get("/videos/:id", function (req, res){
